bb52e146cd6ae4a080c542d0a6cbc9503df62b26
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2025-08-11 15:00:05 +1000

    Revert "Potentially reduce tailwind css size?"
    This reverts commit 98c6f5844ee379f67d96f676761453d45aef6f6d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: "jit",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   plugins: [],
-  theme: {
-    extend: {},
-    colors: {
-      white: "#ffffff",
-      blue: {
-        600: "#3B82F6",
-      },
-      gray: {
-        100: "oklch(96.7% 0.003 264.542)",
-        200: "oklch(92.8% 0.006 264.531)",
-        500: "oklch(55.1% 0.027 264.364)",
-        900: "oklch(21% 0.034 264.665)",
-      },
-    },
-    fontFamily: {},
-  },
 };
